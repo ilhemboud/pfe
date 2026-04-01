3e8cc2805402491dff0a6cda1f6caf0a64ca5d8c
ilhemboud05 <ilhemmiliboud@gmail.com>
2026-04-02 00:13:01 +0100

    ajout de la methode rechercher par email dans utilisateurRepository.java

--- src/main/java/com/pfe/pfe/repository/UtilisateurRepository.java
+++ src/main/java/com/pfe/pfe/repository/UtilisateurRepository.java
@@ -1,10 +1,14 @@
 package com.pfe.pfe.repository;
 
 import com.pfe.pfe.entity.Utilisateur;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
-
+   Optional<Utilisateur> findByEmailUtil(String email);
+        
 }
