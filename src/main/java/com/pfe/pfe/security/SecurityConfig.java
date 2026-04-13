691fceb468fd53ee7b5a52db0d0ac4d393a18f56
abdellahhebbache <abdellahhebbache7gmail.com>
2026-04-13 23:20:34 +0200

    api

--- src/main/java/com/pfe/pfe/security/SecurityConfig.java
+++ src/main/java/com/pfe/pfe/security/SecurityConfig.java
@@ -38,9 +38,9 @@ private UserDetailServiceImpl userDetailsService;
                 // routes publiques
                 .requestMatchers("/auth/login", "/auth/register/client", "/auth/register/employe" ).permitAll()
                 // routes privées
-                .requestMatchers("/Api/employes/").hasAnyRole("ADMIN", "EMPLOYE")
-                .requestMatchers("/Api/clients/").hasAnyRole("ADMIN", "EMPLOYE")
-                .requestMatchers("/Api/utilisateurs/").hasRole("ADMIN")
+                .requestMatchers("/Api/employes/**").hasAnyRole("ADMIN", "EMPLOYE")
+                .requestMatchers("/Api/clients/**").hasAnyRole("ADMIN", "EMPLOYE")
+                .requestMatchers("/Api/utilisateurs/**").hasRole("ADMIN")
                 // tout le reste nécessite une authentification
                 .anyRequest().authenticated()
             )
